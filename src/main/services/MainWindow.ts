d64057182f92dc34aecf29594c4b12f43b88c39d
omegion <kurtulus@mail.com>
2020-12-28 11:37:04 +0100

    cleanup for typescript

--- src/main/services/MainWindow.ts
+++
@@ -1,9 +0,0 @@
-export default class MainWindow {
-  constructor() {
-    console.log("hola!");
-  }
-
-  static _is_production(): boolean {
-    return process.env.NODE_ENV === "production";
-  }
-}
--- src/renderer/plugins/sentry.js
+++
@@ -1,8 +0,0 @@
-import Setting from '~/models/Setting'
-
-export default function (app) {
-  const setting = Setting.get('analytics_enabled')
-  if (setting === null || setting === 'false') {
-    app.$sentry.close()
-  }
-}
